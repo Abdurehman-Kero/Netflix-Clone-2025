68f068fa0a9982854032cb97023e670793f7889e
Abdurehman Kero <keroabdurehman@gmail.com>
2025-11-29 23:57:21 +0300

    modular style for heaer component added

--- Netflix-Clone/src/Components/Header/Header.jsx
+++ Netflix-Clone/src/Components/Header/Header.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import styles from "./Header.module.css"
 const Header = () => {
   return (
-    <div>Header</div>
-  )
+    <>
+      <div className={`${styles.headerContainer}`}>
+        <ul>
+          <li>Netflix</li>
+          <li>Home</li>
+          <li>TVShows</li>
+          <li>Movies</li>
+          <li>Latest</li>
+          <li>MyList</li>
+          <li>Browse by Languages</li>
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default Header
