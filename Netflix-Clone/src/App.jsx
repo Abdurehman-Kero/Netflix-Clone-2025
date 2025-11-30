622f74742212fa2869f38566f252118d850a3e36
Abdurehman Kero <keroabdurehman@gmail.com>
2025-11-30 18:53:54 +0300

    Header styling added

--- Netflix-Clone/src/App.jsx
+++ Netflix-Clone/src/App.jsx
@@ -7,8 +7,7 @@ import Home from './Pages/Home/Home'
 function App() { 
 
   return (
-    <>
-       <h1>netflix</h1>
+    <> 
        <Home/>
      
     </>
--- Netflix-Clone/src/Components/Header/Header.jsx
+++ Netflix-Clone/src/Components/Header/Header.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import styles from "./Header.module.css"
+ 
+import NetflixLogo from "../../assets/netflix.svg";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import styles from "./Header.module.css";
+
 const Header = () => {
   return (
     <>
       <div className={`${styles.headerContainer}`}>
-        <ul>
-          <li>Netflix</li>
+        <ul className={`${styles.LeftHeaderList}`}>
+          <li><img src={NetflixLogo} alt="" /></li>
           <li>Home</li>
           <li>TVShows</li>
           <li>Movies</li>
@@ -13,9 +19,24 @@ const Header = () => {
           <li>MyList</li>
           <li>Browse by Languages</li>
         </ul>
+
+        <ul className={`${styles.headerRight}`}>
+          <li>
+            <SearchIcon />
+          </li>
+          <li>
+            <NotificationsNoneIcon />
+          </li>
+          <li>
+            <AccountBoxIcon />
+          </li>
+          <li>
+            <ArrowDropDownIcon />
+          </li>
+        </ul>
       </div>
     </>
   );
-}
+};
 
-export default Header
+export default Header;
