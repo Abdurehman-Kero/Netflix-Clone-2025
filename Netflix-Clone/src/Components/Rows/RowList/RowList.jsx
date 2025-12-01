21bbc101807493b6d2cca42fb8f0045c81abed34
Abdurehman Kero <keroabdurehman@gmail.com>
2025-12-01 14:42:56 +0300

    rows folder added

---
+++ Netflix-Clone/src/Components/Rows/RowList/RowList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Row from '../Row/Row'
+
+const RowList = () => {
+  return (
+    <>
+    <Row/>
+    </>
+  )
+}
+
+export default RowList
--- Netflix-Clone/src/Pages/Home/Home.jsx
+++ Netflix-Clone/src/Pages/Home/Home.jsx
@@ -2,12 +2,14 @@
 import Header from '../../Components/Header/Header'
 import Footer from '../../Components/Footer/Footer'
 import Banner from '../../Banner/Banner'
+import RowList from '../../Components/Rows/RowList/RowList'
 
 const Home = () => {
   return (
      <>
      <Header/>
      <Banner/>
+     <RowList/>
      <Footer/>
      </>
   )
